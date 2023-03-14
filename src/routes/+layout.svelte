c16d3884ab2a07197ab4ebf3d6c8a9e4263784c6
Fluxticks <benjigarment.appdev@gmail.com>
2023-03-14 12:15:34 +0100

    Implemented darkMode store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,15 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+	import { siteDarkMode } from '../stores.js';
+
+	siteDarkMode.subscribe((value) => {
+		const classList = document.documentElement.classList;
+		if (value) {
+			if (!classList.contains('dark')) document.documentElement.classList.add('dark');
+		} else {
+			if (classList.contains('dark')) document.documentElement.classList.remove('dark');
+		}
+	});
+</script>
+
+<slot />
